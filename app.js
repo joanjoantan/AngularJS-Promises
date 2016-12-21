5038969d82b19d09a9b366293083c4eb49ff2f10
Joanne Tan <joanjoantan@users.noreply.github.com>
2016-12-21 17:09:31 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 
+angular.module('myModule', [])
+
+  .factory('HelloWorld', function($q, $timeout) {
+  
+    var getMessages = function() {
+      var deferred = $q.defer();
+  
+      $timeout(function() {
+        deferred.resolve(['Hello', 'world']);
+      }, 2000);
+  
+      return deferred.promise;
+    };
+    
+    return {
+      getMessages: getMessages
+    };
+  
+  })
+  
+  .controller('HelloCtrl', function($scope, HelloWorld) {
+  
+    $scope.messages = HelloWorld.getMessages();
+  
+  });
